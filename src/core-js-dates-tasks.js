69823397c280ea77632f64a65467e23169ff7a74
Kanstantsin Davydovich <kostiarabota@gmail.com>
2024-03-22 09:36:37 +0300

    feat: add formatDate function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -169,8 +169,13 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const dt = new Date(date);
+  const year = dt.getFullYear();
+  const month = dt.getMonth();
+  const day = dt.getUTCDate();
+  const time = dt.toLocaleTimeString('en-US', { timeZone: 'UTC' });
+  return `${month + 1}/${day}/${year}, ${time}`;
 }
 
 /**
